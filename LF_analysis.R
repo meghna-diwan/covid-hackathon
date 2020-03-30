7c3f56dd406e6123355933457bab553407e63366
Luis Flosi <54824168+LuisFlosi@users.noreply.github.com>
2020-03-30 13:10:06 -0500

    Analysis of exponential rates before and after government measures

--- LF_analysis.R
+++ LF_analysis.R
@@ -6,6 +6,8 @@ library(tidyr)
 library(dplyr)
 `%nin%` = Negate(`%in%`)
 
+# Resctructuce data to include all dates for all states, filling missing with 0 new cases and grouping cities into their respective states
+
 dat = read.csv("C:/Users/luiseduardo/OneDrive/Documentos/MScA/0. Side Projects/COVID-19/covid-hackathon/us_covid19_w_measures_and_risk_pop.csv")
 dat["X"] = NULL
 dat["Date"] = lapply(dat["Date"],anytime)
@@ -83,9 +85,11 @@ write.csv(dat,"C:/Users/luiseduardo/OneDrive/Documentos/MScA/0. Side Projects/CO
 
 states_daily = read.csv("C:/Users/luiseduardo/OneDrive/Documentos/MScA/0. Side Projects/COVID-19/covid-hackathon/us_covid19_w_measures_and_risk_pop_lfFix.csv")
 
+# Create columns for future confirmed cases on each day
+
 dt <- as.data.table(states_daily)
 setkey(dt, State, Date)
-dt[, new_confirmed := Confirmed - shift(Confirmed, fill = first(Confirmed)), by = State]
+dt[, new_confirmed := Confirmed]
 dt[, new_confirmed_next_day := shift(new_confirmed, n = 1, type = "lead"), by = State]
 dt[, new_confirmed_next_2days := shift(new_confirmed, n = 2, type = "lead"), by = State]
 dt[, new_confirmed_next_3days := shift(new_confirmed, n = 3, type = "lead"), by = State]
@@ -94,10 +98,111 @@ dt[, new_confirmed_next_7days := shift(new_confirmed, n = 7, type = "lead"), by 
 
 states_daily = as.data.frame(dt)
 
-il_daily = states_daily[states_daily["State"] == "Illinois",]
+st_daily = states_daily[states_daily["State"] == "Alabama",]
 
-ggplot(data = il_daily, aes(Date, new_confirmed)) +
+ggplot(data = st_daily, aes(Date, new_confirmed)) +
   geom_bar(stat="identity")
 
+library(faraway)
+library(MASS)
+library(lmtest)
+library(fitdistrplus)
 
+# Create list of exponential fits for all states and measures, before and after, using exponential curve
 
+states = unique(states_daily$State)
+states = states[states != "Unassigned Location"]
+states_results = list()
+p = 1
+for (j in states){
+  st_daily = states_daily[states_daily["State"] == j,]
+  effects_list = list()
+  k = 1
+  for (i in c(5:11,13:15)){
+    if (sum(as.numeric(as.character(st_daily[,i]))) == 0){
+      effects_list[[k]] = NA
+      k = k+1
+      next
+    } else {
+    before = st_daily[st_daily[,i] == 0,]
+    if (sum(before$Confirmed) == 0){
+      effects_list[[k]] = NA
+      k = k + 1
+      next
+    } else {
+    before_distr = fitdist(before$Confirmed,"exp")
+    before_rate = before_distr$estimate
+    before_aic = before_distr$aic
+    before_test = ks.test(before$Confirmed,"pexp",rate=before_distr$estimate[1])
+    after = st_daily[st_daily[,i] == 1,]
+    tryCatch(
+      expr = {
+        after_distr = fitdist(after$Confirmed,"exp")
+        after_rate = after_distr$estimate
+        after_aic = after_distr$aic
+        after_test = ks.test(after$Confirmed,"pexp",rate=after_distr$estimate[1])  
+      },
+      error = function(e){
+        after_distr = "unable to calculate"
+        after_rate = "unable to calculate"
+        after_aic = "unable to calculate"
+        after_test = "unable to calculate"
+      }
+    )
+    effects_list[[k]] = list(before_distr = before_distr,
+                           before_rate = before_rate,
+                           before_aic = before_aic,
+                           before_test = before_test,
+                           after_distr = after_distr,
+                           after_rate = after_rate,
+                           after_aic = after_aic,
+                           after_test = after_test)
+    k = k + 1
+    }
+    }
+  }
+  states_results[[p]] = effects_list
+  p = p + 1
+  }
+
+names(states_results) = states
+for (i in names(states_results)){
+  names(states_results[[i]]) = names(st_daily[,c(5:11,13:15)])
+}
+# Create list of exponential rates for good fits before and after (10% cutoff)
+
+rates = list()
+for (i in names(states_results)){
+  for (j in names(states_results[[i]])){
+    if (j %nin% names(rates)) {
+      rates[[j]] = list(before = c(), after = c())
+    }
+    if (is.na(states_results[[i]][[j]])){
+      next
+    } else {
+      if ((states_results[[i]][[j]]$before_test$p.value < 0.1) & (states_results[[i]][[j]]$after_test$p.value < 0.1)){
+        rates[[j]]$before = append(rates[[j]]$before,states_results[[i]][[j]]$before_rate[[1]])
+        rates[[j]]$after = append(rates[[j]]$after,states_results[[i]][[j]]$after_rate[[1]])
+      }
+    }
+  }
+}
+
+# Same list but cutoff only applies to before (since we don't have much data for "after" casese yet)
+
+rates_before_cutoff = list()
+for (i in names(states_results)){
+  for (j in names(states_results[[i]])){
+    if (j %nin% names(rates_before_cutoff)) {
+      rates_before_cutoff[[j]] = list(before = c(), after = c())
+    }
+    if (is.na(states_results[[i]][[j]])){
+      next
+    } else {
+      if ((states_results[[i]][[j]]$before_test$p.value < 0.1) & (states_results[[i]][[j]]$after_test$p.value < 0.2)){
+        rates_before_cutoff[[j]]$before = append(rates_before_cutoff[[j]]$before,states_results[[i]][[j]]$before_rate[[1]])
+        rates_before_cutoff[[j]]$after = append(rates_before_cutoff[[j]]$after,states_results[[i]][[j]]$after_rate[[1]])
+      }
+    }
+  }
+}
